26ff8b975a4df1777939a749947cee0bbae84d4a
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-06-26 20:41:53 -0400

    server.js set up with routes

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 const express = require('express')
+const app = express();
+app.use(express.json()) 
+
+const artistRouter = require('./routes/artistRouter');
+const albumRouter = require('./routes/albumRouter');
+const trackRouter = require('./routes/trackRouter');
+const labelRouter = require('./routes/labelRouter');
+
+app.use('/artist', artistRouter);
+app.use('/album', albumRouter);
+app.use('/track', table3Router);
+app.use('/label', labelRouter);
+
+app.listen(3000, (error) => {
+    if (error) {
+      console.log("Error while starting the server:", error);
+    } else {
+      console.log("Server started on port 3000");
+    }
+});
