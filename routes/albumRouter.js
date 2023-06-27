ac7282d93cd8917568964781805cf0369c2629ac
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-06-26 21:37:31 -0400

    albumRouter setup completed --> testing done

---
+++ routes/albumRouter.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+// the 'pool' used to interact with the db
+const con = require('./dbCon');
+
+router.post('/post', (req, res) => {
+    const artist_id = req.body.artist_id;
+    const title = req.body.title;
+    const release_date = new Date(req.body.release_date);
+    res.send("Values successfully received.");
+
+    con.query('INSERT INTO Album (artist_id, title, release_date) VALUES ($1, $2, $3)', [artist_id, title, release_date], (error, result) => {
+        if (error) {
+            console.log("Not able to post values into table: " + error);
+        } else {
+            console.log("Values successfully inserted into table.");
+        }
+    });
+});
+
+router.get('/fetch', (req, res) => {
+    con.query('SELECT * FROM Artist', (error, result) => {
+        if (error) {
+            console.log(error);
+            res.send("Database error");
+        } else {
+            let r = result.rows;
+            if (r.length >= 2) {
+                res.send([r[r.length - 2], r[r.length - 1]]);
+            } else {
+                res.send(r);
+            }
+        }
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 app.use(express.json()) 
 
 const artistRouter = require('./routes/artistRouter');
-// const albumRouter = require('./routes/albumRouter');
+const albumRouter = require('./routes/albumRouter');
 // const trackRouter = require('./routes/trackRouter');
 // const labelRouter = require('./routes/labelRouter');
 
 app.use('/artist', artistRouter);
-// app.use('/album', albumRouter);
+app.use('/album', albumRouter);
 // app.use('/track', trackRouter);
 // app.use('/label', labelRouter);
 
